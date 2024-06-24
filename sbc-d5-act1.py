0ce889405de812807789f6de5657c9273d978e7d
JhorenDa <jhoren.pana@aclcbutuan.edu.ph>
2024-06-24 21:13:31 +0800

    first commit

--- sbc-d5-act1.py
+++ sbc-d5-act1.py
@@ -1,34 +1,22 @@
 num = []
 
-def naa_add():
-    number = int(input("Enter number: "))
-    num.append(number)
-
-def naa_delete():
-    num.pop(0)
-
-def wala_add():
-    number = int(input("Enter number: "))
-    num.insert(0, number)
-
-def wala_delete():
-    num.pop()
-
 while True:
     print("1-Add(Naa), 2-Delete(Naa), 3-Add(Wala), 4-Delete(Wala), 5-Exit")
     choice = input("Enter your choice: ")
 
     if choice == "1":
-        naa_add()
+        number = int(input("Enter number: "))
+        num.append(number)
 
     elif choice == "2":
-        naa_delete()
+        num.pop(0)
 
     elif choice == "3":
-        wala_add()
+        number = int(input("Enter number: "))
+        num.insert(0, number)
 
     elif choice == "4":
-        wala_delete()
+        num.pop()
 
     elif choice == "5":
         print("bye.")
